0b7682c72d2423bb8f422be9853ea5e561320ba8
orenAzuz <orezaz@gmail.com>
2019-08-01 10:18:25 +0300

    Update MainService.java

--- ChuckYodis/src/main/java/com/orez/services/MainService.java
+++ ChuckYodis/src/main/java/com/orez/services/MainService.java
@@ -33,36 +33,35 @@ public class MainService {
 	@Autowired
 	private MongoOperations mongoOperations;
 	RestTemplate restTemplate = new RestTemplate();
-    HttpHeaders headers = new HttpHeaders();
+        HttpHeaders headers = new HttpHeaders();
    
 	
 	public ChackYodaFacts getChuckNorrisRundom(){
 		
 	    final String uri = "https://api.chucknorris.io/jokes/random";
 	    headers.setAccept(Arrays.asList(MediaType.APPLICATION_JSON));
-	    headers.add("user-agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/54.0.2840.99 Safari/537.36");
+	    headers.add("user-agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 "+
+			"(KHTML, like Gecko) Chrome/54.0.2840.99 Safari/537.36");
 	    HttpEntity<String> entity = new HttpEntity<String>("parameters", headers);
 	    
-        ChackNorris result = restTemplate.exchange(uri, HttpMethod.GET,entity,ChackNorris.class).getBody();
+            ChackNorris result = restTemplate.exchange(uri, HttpMethod.GET,entity,ChackNorris.class)
+		    .getBody();
 		String resultString = "";
 		ChackYodaFacts chack = new ChackYodaFacts();
-        try {
-        	resultString = yodaTranslate(result.getValue());
-        	 
-		    } 
-        catch(Exception e) {
-        	
-        	resultString = result.getValue();  
-			System.out.println(e.getMessage());
-		  }
+      
+		
+	    try { resultString = yodaTranslate(result.getValue()); } 
+		
+            catch(Exception e) {
+        	   resultString = result.getValue();  
+		   System.out.println(e.getMessage());
+		       }
             
-            chack.setFact_text(resultString);
+                chack.setFact_text(resultString);
         	chack.setEnterd_date(new Date());
         	
         	chack.setId((int) this.mongoOperations.count(null, ChackYodaFacts.class));
-			this.mongoOperations.save(chack);
-			
-	
+		this.mongoOperations.save(chack);
 	    return chack;
 	}
 	
@@ -73,16 +72,14 @@ public class MainService {
 		String uri =  url+value;
 		
 		CloseableHttpClient httpClient = HttpClients.custom()
-                .setSSLHostnameVerifier(new NoopHostnameVerifier())
-                .build();
-			HttpComponentsClientHttpRequestFactory requestFactory = new HttpComponentsClientHttpRequestFactory();
-			requestFactory.setHttpClient(httpClient);
-			RestTemplate restTemplate = new RestTemplate(requestFactory);
+                      .setSSLHostnameVerifier(new NoopHostnameVerifier())
+                      .build();
+		HttpComponentsClientHttpRequestFactory requestFactory = 
+			new HttpComponentsClientHttpRequestFactory();
+		requestFactory.setHttpClient(httpClient);
+		RestTemplate restTemplate = new RestTemplate(requestFactory);
 
 	        Translate result = restTemplate.getForObject(uri,Translate.class);
-
-		   
-		System.out.println(result.getContents().getText());
 		
 		return result.getContents().getText();
 	}
@@ -91,7 +88,8 @@ public class MainService {
 	public List<ChackYodaFacts> getFacts(){
 
 		List<ChackYodaFacts>facts = this.mongoOperations.findAll(ChackYodaFacts.class);
-		
 		return facts;
-	}
+		}
+	
+	
 }
